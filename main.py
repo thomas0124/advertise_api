d0fce34a55e5418056459fb8bd1a1292dc80cfe9
トーマス <115143247+thomas0124@users.noreply.github.com>
2023-12-07 00:29:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI, File, UploadFile
-import easyocr
 from PIL import Image
 import io
 
 app = FastAPI()
-
-reader = easyocr.Reader(['en'])  # 'en'は英語の言語コードです
 
 def convert_to_grayscale(file):
     # 画像を読み込む
@@ -33,16 +30,3 @@ def read_root():
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
     return {"filename": file.filename}
-
-@app.post("/extract_text/")
-async def extract_text_from_image(file: UploadFile = File(...)):
-    contents = await file.read()
-    image = Image.open(io.BytesIO(contents))
-
-    # 画像からテキストを抽出
-    result = reader.readtext(image)
-
-    # OCRの結果を抽出したテキストに変換
-    extracted_text = ' '.join([entry[1] for entry in result])
-
-    return {"extracted_text": extracted_text}
