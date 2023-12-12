4b2850f5d98abde67f5fbfa808a477254aee93ed
トーマス <115143247+thomas0124@users.noreply.github.com>
2023-12-12 22:12:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,6 @@ import numpy as np
 
 app = FastAPI()
 
-def convert_to_grayscale(file):
-    # 画像を読み込む
-    img = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
-
-    # グレースケールに変換
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # グレースケール画像をバイトデータに変換
-    retval, buffer = cv2.imencode('.jpg', gray_img)
-    gray_img_bytes = buffer.tobytes()
-
-    return gray_img_bytes
-
-@app.post("/")
-async def convert_to_grayscale_api(file: UploadFile = File(...)):
-    gray_img_bytes = convert_to_grayscale(file)
-
-    return StreamingResponse(io.BytesIO(gray_img_bytes), media_type="image/jpeg")
-
 @app.get("/")
 def read_root():
     return {"message": "Hello World!"}
