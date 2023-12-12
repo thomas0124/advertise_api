f64c2e6c6c7e756c22405868022b7aa796341c9b
トーマス <115143247+thomas0124@users.noreply.github.com>
2023-12-12 22:23:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
 from fastapi import FastAPI, File, UploadFile
-import easyocr
 from PIL import Image
 import io
 import cv2
 from test import get_status
 
 app = FastAPI()
-
-reader = easyocr.Reader(['en'])  # 'en'は英語の言語コードです
-
-
-@app.post("/extract_text/")
-async def extract_text_from_image(file: UploadFile = File(...)):
-    contents = await file.read()
-    image = Image.open(io.BytesIO(contents))
-
-    # 画像からテキストを抽出
-    result = reader.readtext(image)
-
-    # OCRの結果を抽出したテキストに変換
-    extracted_text = ' '.join([entry[1] for entry in result])
-
-    return {"extracted_text": extracted_text}
 
 @app.post("/status/")
 async def status(file: UploadFile = File(...)):
