d006a9e9c471cc45ce5fab93f051add2fa13bfcb
トーマス <115143247+thomas0124@users.noreply.github.com>
2023-12-12 22:13:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import StreamingResponse
-from PIL import Image
-import io
-import cv2
-import numpy as np
 
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello World!"}
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
